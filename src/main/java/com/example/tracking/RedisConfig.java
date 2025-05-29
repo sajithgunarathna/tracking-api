fbaa1f35f8ccfbf056d1474362dd3c71ed1d92ae
Chinthaka1 <sajithdoo@gmail.com>
2025-05-29 19:48:03 +0800

    adding custom redis template

---
+++ src/main/java/com/example/tracking/RedisConfig.java
@@ -0,0 +1,45 @@
+package com.example.tracking;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.redis")
+public class RedisConfig {
+
+    private String host;
+    private int port;
+    private String username;
+    private String password;
+    private boolean sslEnabled;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(host);
+        redisConfig.setPort(port);
+        redisConfig.setUsername(username);
+        redisConfig.setPassword(RedisPassword.of(password));
+
+        return new LettuceConnectionFactory(redisConfig);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+
+    // setters for @ConfigurationProperties
+    public void setHost(String host) { this.host = host; }
+    public void setPort(int port) { this.port = port; }
+    public void setUsername(String username) { this.username = username; }
+    public void setPassword(String password) { this.password = password; }
+    public void setSslEnabled(boolean sslEnabled) { this.sslEnabled = sslEnabled; }
+}
